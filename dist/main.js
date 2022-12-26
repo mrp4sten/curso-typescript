316fea860e80ebd0ec56f423c4451ffc7a36530f
pacomastermind <paco@mastermind.ac>
2022-12-26 11:22:05 +0100

    c2v7

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const i=[];function o(i){console.log(i)}function n(n){i.push(n),o("[NUEVO] "+n)}var l;n("Nigiri salmon"),n("California roll"),l="Nigiri salmon",i.splice(l,1),o("[BORRADO] "+l)})();
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const pedido=[];
+
+function log(texto){
+    console.log(texto);
+}
+
+function nuevoElm(elm){
+    pedido.push(elm);
+    log("[NUEVO] "+elm)
+}
+
+function borrarElm(ind){
+    pedido.splice(ind,1);
+    log("[BORRADO] "+ind)
+}
+
+nuevoElm("Nigiri salmon");
+nuevoElm("California roll");
+borrarElm("Nigiri salmon");
