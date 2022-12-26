a2379728a6b964ed01915ddcd24440dc54503ac3
pacomastermind <paco@mastermind.ac>
2022-12-26 11:46:42 +0100

    c2v8

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{const i=[];function o(i){console.log(i)}function n(n){i.push(n),o("[NUEVO] "+n)}var l;n("Nigiri salmon"),n("California roll"),l="Nigiri salmon",i.splice(l,1),o("[BORRADO] "+l)})();
