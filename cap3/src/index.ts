a5301684560a844be7f29c629bd6b26a9963ed6c
pacomastermind <paco@mastermind.ac>
2023-01-12 12:10:19 +0100

    c3v19

--- cap3/src/index.ts
+++ cap3/src/index.ts
@@ -1,15 +1,35 @@
 //Definicion de las variables
 let nombre:string="Paco";
 let edad:number=40;
-let peso:number=80;
-let altura:number=175;
 let sexo:boolean=true;
+//Captura de inf a partir del formulario
+const pesoInput:HTMLInputElement=<HTMLInputElement>document.getElementById("peso");
+const alturaInput:HTMLInputElement=<HTMLInputElement>document.getElementById("altura");
+const botonCalculo:HTMLButtonElement=<HTMLButtonElement>document.getElementById("botonCalculo");
 
-//Calculo del IMC
-function calcularIMC(peso:number,altura:number):number{
-    return peso/(altura*altura);
+//Transformacion de datos de formulario
+function transformar(elemento:HTMLInputElement):number{
+    let valor:number|any;
+
+    valor=parseFloat(elemento.value);
+    if(isNaN(valor)) valor=0;
+    return valor;
 }
 
-let imc=calcularIMC(peso,altura);
+//Calculo del IMC
+function calcularIMC(event:any){
+    let peso:number;
+    let altura:number;
 
-console.log("El calculo del IMC: "+imc);
+    event?.preventDefault();
+
+    peso=transformar(pesoInput);
+    altura=transformar(alturaInput);
+
+    console.log(peso);
+    console.log(altura);
+}
+
+//Evento de calculo
+botonCalculo.addEventListener("click",calcularIMC);
+
