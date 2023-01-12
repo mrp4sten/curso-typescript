5f01370cc6308ffe80f304bc232ba8d5a2f3d271
pacomastermind <paco@mastermind.ac>
2023-01-12 11:27:28 +0100

    c3v18

--- cap3/dist/js/index.js
+++ cap3/dist/js/index.js
@@ -1,11 +1,23 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
+
+//Definicion de las variables
 let nombre = "Paco";
 let edad = 40;
+let peso = 80;
+let altura = 175;
+let sexo = true;
+//Calculo del IMC
+function calcularIMC(peso, altura) {
+    return peso / (altura * altura);
+}
+let imc = calcularIMC(peso, altura);
+console.log("El calculo del IMC: " + imc);
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9qcy9pbmRleC5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wZXNvaWRlYWwvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsibGV0IG5vbWJyZSA9IFwiUGFjb1wiO1xyXG5sZXQgZWRhZCA9IDQwO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9qcy9pbmRleC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBYTtBQUNiO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Blc29pZGVhbC8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcclxuLy9EZWZpbmljaW9uIGRlIGxhcyB2YXJpYWJsZXNcclxubGV0IG5vbWJyZSA9IFwiUGFjb1wiO1xyXG5sZXQgZWRhZCA9IDQwO1xyXG5sZXQgcGVzbyA9IDgwO1xyXG5sZXQgYWx0dXJhID0gMTc1O1xyXG5sZXQgc2V4byA9IHRydWU7XHJcbi8vQ2FsY3VsbyBkZWwgSU1DXHJcbmZ1bmN0aW9uIGNhbGN1bGFySU1DKHBlc28sIGFsdHVyYSkge1xyXG4gICAgcmV0dXJuIHBlc28gLyAoYWx0dXJhICogYWx0dXJhKTtcclxufVxyXG5sZXQgaW1jID0gY2FsY3VsYXJJTUMocGVzbywgYWx0dXJhKTtcclxuY29uc29sZS5sb2coXCJFbCBjYWxjdWxvIGRlbCBJTUM6IFwiICsgaW1jKTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
--- cap3/src/index.ts
+++ cap3/src/index.ts
@@ -1,9 +1,15 @@
+//Definicion de las variables
 let nombre:string="Paco";
 let edad:number=40;
 let peso:number=80;
 let altura:number=175;
 let sexo:boolean=true;
 
-let imc=peso/(altura*altura);
+//Calculo del IMC
+function calcularIMC(peso:number,altura:number):number{
+    return peso/(altura*altura);
+}
+
+let imc=calcularIMC(peso,altura);
 
 console.log("El calculo del IMC: "+imc);
